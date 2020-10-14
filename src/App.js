cf7574caf822bacbc9324d809d72d2142e3c62b0
unknown <tompsherman@gmail.com>
2020-10-13 21:49:48 -0400

    styling and rendering

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react'
 
 import './App.css'
 import LoginButton from './components/LoginButton.jsx'
+import LogoutButton from './components/LogoutButton'
+import Profile from "./components/Profile"
+import {useAuth0} from "@auth0/auth0-react"
 
 const App = () => {
+  const { isLoading } = useAuth0()
+  if (isLoading)
+    return <div>Loading...</div>
+  
   return (
     <>
       <LoginButton />
+      <LogoutButton />
+      <Profile />
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import {Auth0Provider} from '@auth0/auth0-react'
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
-console.log(domain, clientId)
-
 ReactDOM.render(
   <Auth0Provider 
     domain={domain}
